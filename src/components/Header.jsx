13a29547709426d6321f6316ac0b7fb9634f58df
seraheo <heosera76@gmail.com>
2022-08-24 02:34:07 +0900

    로그인 모달창 구현중

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,111 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    height: '50%',
+  },
+};
+
 const Header = ({ blogName }) => {
-  return <div className="h-16 bg-purple-500 text-lg">{blogName}</div>;
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <>
+      <div className="h-16 bg-purple-500 text-4xl flex flex-row items-center justify-between">
+        <div>
+          <a>Quessr</a>
+        </div>
+        <div className="flex flex-row items-center">
+          <button>
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </button>
+          <a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </a>
+          <button onClick={openModal}>로그인</button>
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <div>
+              <img src='https://ifh.cc/g/9LPQbS.jpg'></img>
+              <div className="text-7xl">환영합니다!</div>
+            </div>
+            <div>
+              <h2
+                ref={(_subtitle) => (subtitle = _subtitle)}
+                className="text-6xl"
+              >
+                로그인
+              </h2>
+              <button onClick={closeModal} className="text-4xl">
+                이메일로 로그인
+              </button>
+              <form >
+                <input type='email'placeholder="이메일을 입력하세요."/>
+                <button>로그인</button>
+              </form>
+              <section>
+                <h4>소셜 계정으로 로그인</h4>
+                <div></div>
+              </section>
+            </div>
+          </Modal>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import HomeContainer from '../components/HomeContainer';
 
 const HomePage = () => {
   return (
-    <div className="h-screen text-5xl bg-purple-100 overflow-y-scroll">
+    <div className="h-screen text-5xl bg-purple-100">
       <HomeContainer />
     </div>
   );
